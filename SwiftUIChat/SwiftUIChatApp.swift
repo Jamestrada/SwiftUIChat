c98c1d4e0120ddd0839ec1cf35088617c0722fe6
James Estrada <Jamestrada@users.noreply.github.com>
2021-11-16 23:32:23 -0600

    Initial Commit

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by James Estrada on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
