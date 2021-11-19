4d90577355af6feda12668f87821722477a9c4b5
James Estrada <Jamestrada@users.noreply.github.com>
2021-11-18 23:35:19 -0600

    finish sign up page

--- SwiftUIChat/ContentView.swift
+++ SwiftUIChat/ContentView.swift
@@ -10,20 +10,47 @@ import SwiftUI
 struct ContentView: View {
     
     @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
     
     var body: some View {
         NavigationView {
             ScrollView {
                 
-                Picker(selection: $isLoginMode, label: Text("Picker here")) {
-                    Text("Login")
-                        .tag(true)
-                    Text("Create Account")
-                        .tag(false)
-                }.pickerStyle(SegmentedPickerStyle())
-                    .padding()
-                
-                Text("Here is my creation account page")
+                VStack {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 64))
+                            .padding()
+                    }
+                    
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                    SecureField("Password", text: $password)
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                }
+                .padding()
             }
             .navigationTitle("Create Account")
         }
